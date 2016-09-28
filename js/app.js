62a8770ca7647527d6f23fe85c31acffe8bbf378
John French <hello@johnfrench.xyz>
2016-09-28 14:18:58 -0400

    Makes Nav responsive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 $(document).foundation();
 
+// Menu
+$('.menu-trigger').on('click', function(e) {
+    e.preventDefault();
+    $('nav ul').slideToggle(200);
+    $('nav ul').toggleClass('is-open');
+    $('nav ul li a').on('click', function(){
+        $('nav ul').hide();
+    });
+});
+
+$(window).on('resize', function() {
+    if (Foundation.MediaQuery.atLeast('medium')) {
+        $('nav ul').css('display', 'block');
+    } else {
+        $('nav ul').css('display', 'none');
+    }
+});
+
 // Smooth Scrolling
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
